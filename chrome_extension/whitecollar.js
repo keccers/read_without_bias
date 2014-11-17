421bad1d79bdc22d848c2aa229af265070c5cb86
Katherine Champagne <kechampagne@gmail.com>
2014-11-16 23:52:20 -0600

    get rid of un-needed files for github pages

--- chrome_extension/whitecollar.js
+++
@@ -1,5 +0,0 @@
-console.log("STARTUPS STARTUPS OMG STARTUPS");
-$("body").children().each(function() {
-    $(this).html($(this).html().replace(/startup|start-up/g,"business").replace(/Startups|Start-ups/g,"Businesses").replace(/startups|start-ups/g,"businesses").replace(/Startup/g,"Business"));
-    console.log("WE OUT!");
-});
